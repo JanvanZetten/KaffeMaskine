e35ffd1fa08a0c25f93fe2e0b097d1fcafaae5bb
JanvanZetten <33052378+JanvanZetten@users.noreply.github.com>
2017-10-24 12:30:22 +0200

    Fungerende kaffestråle

--- KaffemaskineSIM/src/kaffemaskinesim/FXMLDocumentController.java
+++ KaffemaskineSIM/src/kaffemaskinesim/FXMLDocumentController.java
@@ -7,13 +7,16 @@ package kaffemaskinesim;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-/**
+/**#000000
  *
  * @author janvanzetten
  */
@@ -24,6 +27,10 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Rectangle KaffeStraale;
     
+    
+    Timer timer = new Timer();
+    
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -31,23 +38,76 @@ public class FXMLDocumentController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
 
     @FXML
-    private void SortKaffeAction(ActionEvent event) {
+    private void SortKaffeAction(ActionEvent event) throws InterruptedException {
+        KaffeStraale.setFill(Color.BLACK);
+        
+            TimerTask task = new TimerTask()
+    {
+        
+        public void run()
+        {
+                KaffeStraale.setFill(Color.TRANSPARENT);   
+        }
+
+    };
+            
+            
+        timer.schedule(task,10000l);
     }
 
     @FXML
-    private void SortKaffeMedSukkerAction(ActionEvent event) {
+    private void SortKaffeMedSukkerAction(ActionEvent event) throws InterruptedException {
+        KaffeStraale.setFill(Color.BLACK);
+        
+            TimerTask task = new TimerTask()
+    {
+        
+        public void run()
+        {
+                KaffeStraale.setFill(Color.TRANSPARENT);   
+        }
+
+    };
+        
+        timer.schedule(task,10000l);
     }
 
     @FXML
-    private void KaffeMedMælkAction(ActionEvent event) {
+    private void KaffeMedMælkAction(ActionEvent event) throws InterruptedException {
+        KaffeStraale.setFill(Color.SIENNA);
+        
+            TimerTask task = new TimerTask()
+    {
+        
+        public void run()
+        {
+                KaffeStraale.setFill(Color.TRANSPARENT);   
+        }
+
+    };
+        
+        timer.schedule(task,10000l);
     }
 
     @FXML
-    private void KaffeMedMælkOgSukker(ActionEvent event) {
+    private void KaffeMedMælkOgSukker(ActionEvent event) throws InterruptedException {
+        KaffeStraale.setFill(Color.SIENNA);
+        
+            TimerTask task = new TimerTask()
+    {
+        
+        public void run()
+        {
+                KaffeStraale.setFill(Color.TRANSPARENT);   
+        }
+
+    };
+        
+        timer.schedule(task,10000l);
     }
     
 }
